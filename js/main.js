aa78b28ce472ff31d4682931a6cfdd5dac89e228
Paolo <Paolo>
2016-02-13 20:04:28 +0800

    added smooth scroll to main menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function(){
+    $('#main-menu a[href*="#"]:not([href="#"])').click(function() {
+        $(this).closest(".menu-item").addClass("selected").siblings(".menu-item").removeClass("selected");
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    
     $(".menu-toggle").click(function(){
         $($(this).data('menu')).toggleClass('open');
     });
