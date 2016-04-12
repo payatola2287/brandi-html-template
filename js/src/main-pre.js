097593bcd67028647fd1694d79633f154f8def07
payatola2287 <paolo.j.gallardo@gmail.com>
2016-04-12 19:02:41 +0800

    added a src folder to minify the js

---
+++ js/src/main-pre.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function(){
+    windowHeight = $(window).height();
+    $('#main-menu a[href*="#"]:not([href="#"])').click(function() {
+        $(this).closest(".menu-item").addClass("selected").siblings(".menu-item").removeClass("selected");
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 450);
+                return false;
+            }
+        }
+    });
+    
+    $(window).scroll(function(){
+        var scroll = $(this).scrollTop() + 50;
+        if(!$(".main-header").hasClass("scrolled")){
+            if(scroll > windowHeight){
+                $(".main-header").addClass("scrolled");
+            }        
+        }else{
+            if(scroll < windowHeight){
+                $(".main-header").removeClass("scrolled");
+            }
+        }
+        if($("#main-menu").hasClass('open')){
+            $("#main-menu").removeClass('open');
+        }
+    });
+    $('.animated').viewportChecker({
+        classToAdd : 'fadeInUp'
+    });
+    $(".menu-toggle").click(function(){
+        $($(this).data('menu')).toggleClass('open');
+    });
+    $("#works-showcase .row").mixItUp({
+        selectors : {
+            target : '.work'
+        },
+        callbacks : {
+            onMixEnd: function(state){
+                console.log(state.activeFilter);
+            }
+        }
+    });
+});
